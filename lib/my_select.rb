f28507730a00cbbca522b78511885a7d92a47e5a
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2015-08-19 10:38:28 -0400

    Update my_select.rb

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,15 +1,3 @@
-require 'pry'
-
 def my_select(collection)
-	i = 0
-	selection = []
-	if collection.length > 0
-		while i < collection.length
-			if yield(collection[i]) == true
-			    selection << collection[i]
-			end
-			i+=1
-		end
-	end
-	selection
+ # your code here!
 end
